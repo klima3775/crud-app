13a101ca4d984969a5aeccce28a238b69c6de1bf
klima3775 <113065590+klima3775@users.noreply.github.com>
2024-04-17 03:52:50 +0300

    fix padding

--- src/components/employees-info/info.jsx
+++ src/components/employees-info/info.jsx
@@ -5,8 +5,8 @@ const AppInfo = (props) => {
   return (
     <div className="app-info">
       <h1>Працівники компанії</h1>
-      <h2>Загальна кількість працівників{employees}</h2>
-      <h2>Премію отримають{increased} </h2>
+      <h2>Загальна кількість працівників {employees}</h2>
+      <h2>Премію отримають {increased} </h2>
     </div>
   );
 };
