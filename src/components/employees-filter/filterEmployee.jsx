32491bf3e18fc4613c169fdf9a386cf810e079b6
klima3775 <113065590+klima3775@users.noreply.github.com>
2024-04-24 02:50:09 +0300

    edit buttons color

--- src/components/employees-filter/filterEmployee.jsx
+++ src/components/employees-filter/filterEmployee.jsx
@@ -4,16 +4,18 @@ const Filter = (props) => {
   const buttonsData = [
     { name: "all", label: "Всі Працівники" },
     { name: "rise", label: "На підвищення" },
-    { name: "moreThen1000", label: "З/П більше 1000$" },
+    { name: "moreThen1000", label: "З/П більше 1000$", colored: true },
   ];
-  const buttons = buttonsData.map(({ name, label }) => {
+  const buttons = buttonsData.map(({ name, label, colored }) => {
     const active = props.filter === name;
     const clazz = active ? "btn-light" : "btn-outline-light";
+    const style = colored ? { color: "red" } : null;
     return (
       <button
         key={name}
         onClick={() => props.onFilterSelect(name)}
         className={`btn ${clazz}`}
+        style={style}
       >
         {label}
       </button>
