af3188f2960811feb3940b004a9d9862f5e6987a
Klima <klima3775@gmail.com>
2024-04-27 00:28:46 +0300

    Components searchPanel rewrote on hooks

--- src/components/search-panel/searchPanel.jsx
+++ src/components/search-panel/searchPanel.jsx
@@ -1,32 +1,64 @@
+// import "./searchPanel.css";
+// import { Component } from "react";
+// import { debounce } from "lodash";
+
+// class SearchPanel extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       term: "",
+//     };
+//     this.debouncedSearch = debounce(this.props.onUpdateSearch, 300);
+//   }
+
+//   oneSearchPanel = (e) => {
+//     const term = e.target.value.replace(/\s+/g, "");
+//     this.setState({ term });
+//     this.debouncedSearch(term);
+//   };
+
+//   render() {
+//     return (
+//       <input
+//         type="text"
+//         className="form-control search-input"
+//         placeholder="Пошук по працівникам"
+//         value={this.state.term}
+//         onChange={this.oneSearchPanel}
+//       />
+//     );
+//   }
+// }
+// export default SearchPanel;
+import React, { useState, useEffect } from "react";
 import "./searchPanel.css";
-import { Component } from "react";
 import { debounce } from "lodash";
 
-class SearchPanel extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      term: "",
-    };
-    this.debouncedSearch = debounce(this.props.onUpdateSearch, 300);
-  }
+const SearchPanel = ({ onUpdateSearch }) => {
+  const [term, setTerm] = useState("");
 
-  oneSearchPanel = (e) => {
-    const term = e.target.value.replace(/\s+/g, "");
-    this.setState({ term });
-    this.debouncedSearch(term);
+  useEffect(() => {
+    const debouncedSearch = debounce(onUpdateSearch, 300);
+    return () => {
+      debouncedSearch.cancel();
+    };
+  }, [onUpdateSearch]);
+
+  const handleSearch = (e) => {
+    const newTerm = e.target.value.replace(/\s+/g, "");
+    setTerm(newTerm);
+    onUpdateSearch(newTerm);
   };
 
-  render() {
-    return (
-      <input
-        type="text"
-        className="form-control search-input"
-        placeholder="Пошук по працівникам"
-        value={this.state.term}
-        onChange={this.oneSearchPanel}
-      />
-    );
-  }
-}
+  return (
+    <input
+      type="text"
+      className="form-control search-input"
+      placeholder="Пошук по працівникам"
+      value={term}
+      onChange={handleSearch}
+    />
+  );
+};
+
 export default SearchPanel;
