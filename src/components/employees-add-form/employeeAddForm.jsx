3b6c85e5caf391379a23f7f91c69bc5f8760c2b2
Klima <klima3775@gmail.com>
2024-04-26 23:57:39 +0300

    EmployersAddForm rewrote in react hooks

--- src/components/employees-add-form/employeeAddForm.jsx
+++ src/components/employees-add-form/employeeAddForm.jsx
@@ -1,68 +1,64 @@
-import { Component } from "react";
-
+// import { Component } from "react";
+import { useState } from "react";
 import "./addForm.css";
+const EmployersAddForm = (props) => {
+  const [name, setName] = useState("");
+  const [salary, setSalary] = useState("");
 
-class EmployersAddForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "",
-      salary: "",
-    };
-  }
-
-  onValueChange = (e) => {
-    e.preventDefault();
-    this.setState({
-      [e.target.name]: e.target.value,
-    });
+  const onValueChange = (e) => {
+    const value = e.target.value;
+    if (e.target.name === "name") {
+      setName(value);
+    } else if (e.target.name === "salary") {
+      setSalary(value);
+    }
   };
-
-  onSubmit = (e) => {
+  const onSubmit = (e) => {
     e.preventDefault();
-    this.props.onAdd(this.state.name, this.state.salary);
-
-    const newEmployer = { name: this.state.name, salary: this.state.salary };
+    props.onAdd(name, salary);
+    const newEmployer = {
+      name,
+      salary,
+    };
     let users = JSON.parse(localStorage.getItem("users")) || [];
     users.push(newEmployer);
     localStorage.setItem("users", JSON.stringify(users));
+    JSON.parse(localStorage.getItem("users")) || [];
+    users.push(newEmployer);
+    localStorage.setItem("users", JSON.stringify(users));
 
-    this.setState({
-      name: "",
-      salary: "",
-    });
+    setName("");
+    setSalary("");
   };
+  return (
+    <div className="app-add-form">
+      <h3>Додайте нового працівника </h3>
+      <form className="add-form d-flex" onSubmit={onSubmit}>
+        <input
+          type="text"
+          className="form-control new-post-label"
+          placeholder="Як його звати?"
+          name="name"
+          value={name}
+          onChange={onValueChange}
+          required
+        />
+        <input
+          type="number"
+          className="form-control new-post-label"
+          placeholder="З/П в $?"
+          name="salary"
+          value={salary}
+          onChange={onValueChange}
+          required
+        />
 
-  render() {
-    return (
-      <div className="app-add-form">
-        <h3>Додайте нового працівника </h3>
-        <form className="add-form d-flex" onSubmit={this.onSubmit}>
-          <input
-            type="text"
-            className="form-control new-post-label"
-            placeholder="Як його звати?"
-            name="name"
-            value={this.state.name}
-            onChange={this.onValueChange}
-            required
-          />
-          <input
-            type="number"
-            className="form-control new-post-label"
-            placeholder="З/П в $?"
-            name="salary"
-            value={this.state.salary}
-            onChange={this.onValueChange}
-            required
-          />
+        <button type="submit" className="btn btn-outline-light">
+          Добавить
+        </button>
+      </form>
+    </div>
+  );
+};
 
-          <button type="submit" className="btn btn-outline-light">
-            Добавить
-          </button>
-        </form>
-      </div>
-    );
-  }
-}
 export default EmployersAddForm;
