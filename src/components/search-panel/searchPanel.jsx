b62e9b691e7f9d38bd9002fb827398b3c8ca5a54
Klima <klima3775@gmail.com>
2024-04-27 00:29:59 +0300

    fix comment

--- src/components/search-panel/searchPanel.jsx
+++ src/components/search-panel/searchPanel.jsx
@@ -1,35 +1,3 @@
-// import "./searchPanel.css";
-// import { Component } from "react";
-// import { debounce } from "lodash";
-
-// class SearchPanel extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       term: "",
-//     };
-//     this.debouncedSearch = debounce(this.props.onUpdateSearch, 300);
-//   }
-
-//   oneSearchPanel = (e) => {
-//     const term = e.target.value.replace(/\s+/g, "");
-//     this.setState({ term });
-//     this.debouncedSearch(term);
-//   };
-
-//   render() {
-//     return (
-//       <input
-//         type="text"
-//         className="form-control search-input"
-//         placeholder="Пошук по працівникам"
-//         value={this.state.term}
-//         onChange={this.oneSearchPanel}
-//       />
-//     );
-//   }
-// }
-// export default SearchPanel;
 import React, { useState, useEffect } from "react";
 import "./searchPanel.css";
 import { debounce } from "lodash";
